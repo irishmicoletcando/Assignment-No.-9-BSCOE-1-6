679cc0d6d4f68c8c7b2721fe469899a03fd75a57
Irish Micole Cando <irishmicolecando@gmail.com>
2022-02-08 21:16:19 +0800

    Finalizing yt notes needed

--- resume.py
+++ resume.py
@@ -7,19 +7,35 @@
 import json
 from fpdf import FPDF
 
-# P. S: these notes are from the yt vid (https://www.youtube.com/watch?v=q70xzDG6nls) i watched
+with open('resume_info.json') as json_file:
+    dataResume = json.load(json_file)
 
+
+
+
+# P. S: these notes are from the yt vid (https://www.youtube.com/watch?v=q70xzDG6nls) i watched
 # create FPDF object
 # layout ("P", "L")
 # unit ("mm", "cm", "in")
 # format ("A3", "A4", (default), "A5", "Letter", "Legal", (100, 150) -> first integer = width, second integer = height)
 
-pdf = FPDF('P', 'in', 'Letter')
+resumepdf = FPDF('P', 'mm', 'Letter')
 
 # add page
-pdf.add_page()
+resumepdf.add_page()
 
 # specify font 
 # fonts can be times, courier, helvetica, symbol, zpfdingbats
 # 'B' (bold), 'U' (underline), 'I' (italic), '' (regular), combination ('BU')
-pdf.set_font('times', '', 14)
+resumepdf.set_font('times', '', 14)
+
+# adding text
+# w = width
+# h = height
+# txt = text
+# ln = (0 - False, 1 - True = move cursor down to next line)
+# border (0 - False, 1 - True = add border around cell)
+resumepdf.cell(40, 10, "Hello World!", ln=True)
+resumepdf.cell(80, 10, "Goodbye World!")
+
+resumepdf.output("CANDO_IRISH MICOLE.pdf")
