a4b61d392aef87358e115b0ec5baf7bf804f6793
Irish Micole Cando <irishmicolecando@gmail.com>
2022-02-08 09:32:44 +0800

    Added pdf improvements

--- resume.py
+++ resume.py
@@ -4,3 +4,22 @@
 # 	- Your program should read the JSON file and write the details in the PDF
 # 	- The output file should be: LASTNAME_FIRSTNAME.pdf
 
+import json
+from fpdf import FPDF
+
+# P. S: these notes are from the yt vid (https://www.youtube.com/watch?v=q70xzDG6nls) i watched
+
+# create FPDF object
+# layout ("P", "L")
+# unit ("mm", "cm", "in")
+# format ("A3", "A4", (default), "A5", "Letter", "Legal", (100, 150) -> first integer = width, second integer = height)
+
+pdf = FPDF('P', 'in', 'Letter')
+
+# add page
+pdf.add_page()
+
+# specify font 
+# fonts can be times, courier, helvetica, symbol, zpfdingbats
+# 'B' (bold), 'U' (underline), 'I' (italic), '' (regular), combination ('BU')
+pdf.set_font('times', '', 14)
